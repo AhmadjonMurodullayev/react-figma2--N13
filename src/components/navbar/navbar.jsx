1639f585e4d209b3fd366619dd93f30c35d4d117
AhmadjonMurodullayev <murodullayevahmadjon26@gmail.com>
2024-10-03 03:31:44 +0500

    js

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,11 +4,9 @@ import sorch from "../../assets/sorch.svg";
 import { Bar } from "../../assets/icons/bar";
 
 export const Navbar = () => {
-
-
   return (
     <div className={style.navbar}>
-      <nav className="container">
+      <nav className={style.container}>
         <div className={style.navbar_content}>
           <div className={style.navbar_box}>
             <h3 className={style.navbar_title}>О ресноте</h3>
@@ -16,13 +14,16 @@ export const Navbar = () => {
             <h3 className={style.navbar_title}>Наши магазины</h3>
             <h3 className={style.navbar_title}>Контакты</h3>
           </div>
-          
-<div className={style.navbar_bar} >
-<Bar/>
 
-</div>
+          <div className={style.navbar_bar}>
+            <Bar />
+          </div>
           <div className={style.navbar_sorch}>
-            <input className={style.navbar_input} type="text" placeholder="Поиск по сайту" />
+            <input
+              className={style.navbar_input}
+              type="text"
+              placeholder="Поиск по сайту"
+            />
             <img src={sorch} alt="Search" />
           </div>
         </div>
